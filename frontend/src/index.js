d9abb857b547ba2da9acdb59af41a65454afc8f0
jamill1212 <jamiller1256@yahoo.com>
2025-10-18 22:10:09 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,44 @@
-React from "react";
-import ReactDOM from "react-dom/client";
-import "@/index.css";
-import App from "@/App";
+import { useState } from "react";                       import "@/App.css";             import {BrowserRouter,       "react-router-dom";           import Dashboard from       "@/pages/Dashboard";      import DTCScanner from  "@/pages/DTCScanner";          import LiveData from ""@/pages/LiveData";
+import ECUProgramming from   "@/pages/ECUProgramming";     import VehicleInfo from "@/pages/VehicleInfo";       import BottomNav from  "@/components/BottomNav";     import { Toaster } from "@/components/ui/sonner";      
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);"
+function App() {
+  const [currentPage,          setCurrentPage] =             
+useState("dashboard");        
+
+  return (                     
+    <div className="App     
+min-h-screen pb-20">           
+       <Toaster richColors
+position="top-center" />      
+       <BrowseRouter> 
+         <Routes>   
+           <Route path="/" 
+element={<Dashboard />} />
+           <Route path="/dtc"
+element={<DTCScanner />} />
+           <Route.      
+path="/Live-data"       
+element={<LiveData />} /> 
+           <Route path="/ecu"
+element={<ECUProgramming /   >} />         
+           <Route
+path="/live-date"        
+element={<LiveData />} />  
+           <Route path=>} />
+element={<ECUProgramming / 
+>} />     
+           <Route
+path="/vehicle"    
+element={<VehicleInfo />} /> 
+           </Route>    
+           <BottomNav 
+CurrentPage={currentPage}   
+setCurrentPage={s 
+etCurrentPage} /
+>
+      </BrowserRouter>       
+    </div>              
+  );              
+}    
+
+export default App;         
